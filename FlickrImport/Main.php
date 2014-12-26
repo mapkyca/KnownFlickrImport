f23954de401fced0b06c89fcbce3eb492826c5b0
Marcus Povey <marcus@marcus-povey.co.uk>
2014-12-26 12:42:07 +0000

    Import endpoint

--- FlickrImport/Main.php
+++ FlickrImport/Main.php
@@ -7,6 +7,8 @@ namespace IdnoPlugins\FlickrImport {
 	function registerPages() {
 	    \Idno\Core\site()->addPageHandler('account/flickrimport', '\IdnoPlugins\FlickrImport\Pages\Account');
 	    \Idno\Core\site()->template()->extendTemplate('account/menu/items','account/flickrimport/menu');
+	    
+	    \Idno\Core\site()->addPageHandler('flickrimport/import', '\IdnoPlugins\FlickrImport\Pages\Import');
 	}
 
     }
