7c048141884b03e1cd35d15ed6e3c20cedaf18bc
Marcus Povey <marcus@marcus-povey.co.uk>
2014-12-29 10:31:03 +0000

    Flickr import template

---
+++ FlickrImport/templates/email/account/flickrimport.tpl.php
@@ -0,0 +1,12 @@
+<div style="font-weight: bold; font-size: 30px; line-height: 32px; color: #333" align="center">
+    Your Flickr account has been imported!
+</div><br>
+<hr/>
+<br>
+We have finished importing photos and videos from your flickr account!
+<br><br>
+<div align="center">
+    <a href="<?=\Idno\Core\site()->config()->getURL()?>account/flickrimport/" style="background-color:#73B2E3;border:1px solid #73B2E3;border-radius:4px;color:#ffffff;display:inline-block;font-family:sans-serif;font-size:17px;font-weight:normal;line-height:40px;text-align:center;text-decoration:none;width:200px;-webkit-text-size-adjust:none;mso-hide:all;">View log</a>
+</div>
+<br>
+<br><br>
