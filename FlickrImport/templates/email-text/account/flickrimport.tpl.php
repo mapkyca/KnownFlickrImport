45e4af68c87868664b7bbd38d758abf16fb33ad8
Marcus Povey <marcus@marcus-povey.co.uk>
2015-05-24 14:35:37 +0100

    Text only email template

---
+++ FlickrImport/templates/email-text/account/flickrimport.tpl.php
@@ -0,0 +1,6 @@
+Your Flickr account has been imported!
+======================================
+
+We have finished importing photos and videos from your flickr account!
+
+View log: <?=\Idno\Core\site()->config()->getURL()?>account/flickrimport/
